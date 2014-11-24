b32d306c89fb17f0bf6a3fe340f64e24ec01b0f7
Gwilherm <d.gwilherm@yahoo.com>
2014-11-24 22:02:25 +0100

    Added : clone delay command

--- src/game/variables.h
+++ src/game/variables.h
@@ -95,6 +95,7 @@ MACRO_CONFIG_INT(DummyColorFeet, dummy_color_feet, 65408, 0, 0xFFFFFF, CFGFLAG_C
 MACRO_CONFIG_STR(DummySkin, dummy_skin, 24, "default", CFGFLAG_CLIENT|CFGFLAG_SAVE, "Dummy skin")
 MACRO_CONFIG_INT(ClDummy, cl_dummy, 0, 0, 1, CFGFLAG_CLIENT, "0 - player / 1 - dummy")
 MACRO_CONFIG_INT(ClDummyCopyMoves, cl_dummy_copymoves, 0, 0, 1, CFGFLAG_CLIENT, "Whether dummy is copying your moves")
+MACRO_CONFIG_INT(ClDummyCopyMovesDelay, cl_dummy_copymoves_delay, 0, 0, 199, CFGFLAG_CLIENT, "Adds a delay to dummy clone - 0 to 199 ticks (1 tick = 50ms)")
 MACRO_CONFIG_INT(ClDummyHammer, cl_dummy_hammer, 0, 0, 1, CFGFLAG_CLIENT, "Whether dummy is hammering for a hammerfly")
 MACRO_CONFIG_INT(ClDummyResetOnSwitch, cl_dummy_resetonswitch, 0, 0, 1, CFGFLAG_CLIENT|CFGFLAG_SAVE, "Whether dummy should stop pressing keys when you switch")
 
